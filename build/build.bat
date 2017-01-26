bd793e1858ad9a83b668c95150fc8af27c09b8d7
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2017-01-26 11:18:30 -0800

    Switched dependencies to use static versions instead. No more DLLs. Related to #3 and #1

--- build/build.bat
+++ build/build.bat
@@ -13,22 +13,28 @@ set DEVENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\deve
 if not exist %~dp0..\bin mkdir %~dp0..\bin
 
 cd %ZLIB%
+if exist build rmdir /q /s build
 if not exist build mkdir build
 cd build
 cmake .. -G"Visual Studio 14 2015" -DCMAKE_INSTALL_PREFIX=%~dp0../bin/zlib
+cmake --build . --target install --config Debug
 cmake --build . --target install --config Release
 
 cd %LIBPNG%
+if exist build rmdir /q /s build
 if not exist build mkdir build
 cd build
-cmake .. -G"Visual Studio 14 2015" -DCMAKE_INSTALL_PREFIX=%~dp0../bin/libpng -DPNG_BUILD_ZLIB=1 -DZLIB_LIBRARY=%~dp0../bin/zlib/lib/zlib.lib -DZLIB_INCLUDE_DIR=%~dp0../bin/zlib/include -DCMAKE_BUILD_TYPE=Release
+cmake .. -G"Visual Studio 14 2015" -DCMAKE_INSTALL_PREFIX=%~dp0../bin/libpng -DPNG_BUILD_ZLIB=1 -DZLIB_LIBRARY=%~dp0../bin/zlib/lib/zlib.lib -DZLIB_INCLUDE_DIR=%~dp0../bin/zlib/include
+cmake --build . --target install --config Debug
 cmake --build . --target install --config Release
 IF ERRORLEVEL 1 goto ERROR
 
 cd %SDL%
+if exist build rmdir /q /s build
 if not exist build mkdir build
 cd build
-cmake .. -G"Visual Studio 14 2015" -DCMAKE_INSTALL_PREFIX=%~dp0../bin/sdl -DCMAKE_BUILD_TYPE=Release
+cmake .. -G"Visual Studio 14 2015" -DSDL_STATIC=1 -DCMAKE_INSTALL_PREFIX=%~dp0../bin/sdl
+cmake --build . --target install --config Debug
 cmake --build . --target install --config Release
 IF ERRORLEVEL 1 goto ERROR
 
