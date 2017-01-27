8a8e542482207a80f107436577704a8bb9d85476
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2017-01-26 18:53:05 -0800

    Re-enabled SDL_net and updated the build_dependencies.bat to accomodate this change. Submodule now points at custom VS2015 version of SDL_net.

--- build/build_dependencies.bat
+++ build/build_dependencies.bat
@@ -1,16 +1,19 @@
 @echo off
 
-set CMAKE=%~dp0../thirdparty/cmake/3.7.2/bin
+set CMAKE=%~dp0..\thirdparty\cmake\3.7.2\bin
 set PATH=%CMAKE%;%PATH%
 
-set SDL=%~dp0../thirdparty/SDL/2.0.5
-set SDL_net=%~dp0../thirdparty/SDL_net/2.0.1
-set ZLIB=%~dp0../thirdparty/zlib/1.2.11
-set LIBPNG=%~dp0../thirdparty/libpng/1.6.28
+set SDL=%~dp0..\thirdparty\SDL\2.0.5
+set SDL_net=%~dp0..\thirdparty\SDL_net\2.0.1
+set ZLIB=%~dp0..\thirdparty\zlib\1.2.11
+set LIBPNG=%~dp0..\thirdparty\libpng\1.6.28
 
 set DEVENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com"
 
 if not exist %~dp0..\bin mkdir %~dp0..\bin
+
+cd ..
+python src\version.py include/version.h . none
 
 cd %ZLIB%
 if exist build rmdir /q /s build
@@ -37,6 +40,14 @@ cmake .. -G"Visual Studio 14 2015" -DSDL_STATIC=1 -DCMAKE_INSTALL_PREFIX=%~dp0..
 cmake --build . --target install --config Debug
 cmake --build . --target install --config Release
 IF ERRORLEVEL 1 goto ERROR
+
+cd %~dp0
+%DEVENV% %SDL_net%\VisualC\SDL_net.sln /build "Release|Win32"
+if not exist ..\bin\sdl_net mkdir ..\bin\sdl_net
+if not exist ..\bin\sdl_net\lib mkdir ..\bin\sdl_net\lib
+if not exist ..\bin\sdl_net\include mkdir ..\bin\sdl_net\include
+xcopy /q /y %SDL_net%\*.h ..\bin\sdl_net\include
+xcopy /q /y %SDL_net%\VisualC\Win32\Release\SDL2_net.lib ..\bin\sdl_net\lib
 
 goto END
 
--- src/platform/visualc/config.h
+++ src/platform/visualc/config.h
@@ -10,10 +10,10 @@
 #define C_OPENGL 1
 
 /* Define to 1 to enable internal modem support, requires SDL_net */
-#define C_MODEM 0
+#define C_MODEM 1
 
 /* Define to 1 to enable IPX networking support, requires SDL_net */
-#define C_IPX 0
+#define C_IPX 1
 
 /* Enable some heavy debugging options */
 #define C_HEAVY_DEBUG 0
